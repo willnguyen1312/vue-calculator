c78b30a746402aefb2c1d4c01ac37eb54cf2e4c9
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-03-16 14:35:48 +0700

    Initial commit 🎉

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+      includeAssets: ['favicon.ico'],
       manifest: {
         name: 'Vitesse',
         short_name: 'Vitesse',
